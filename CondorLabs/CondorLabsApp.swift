c9a248f870381b38873effd119d2a4c22e6eadf2
Daniel Cano Arbelaez <cano.naposystems@gmail.com>
2021-02-22 09:31:04 -0500

    * Initial commit for technical test -- CondorLabs

---
+++ CondorLabs/CondorLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CondorLabsApp.swift
+//  CondorLabs
+//
+//  Created by Daniel Cano Arbelaez on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CondorLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
